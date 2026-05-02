df7f7da8fc9a283da855f97fe16aeded7b2125fe
Ari <arikrixh@gmail.com>
2026-05-02 17:58:06 +0530

    switch to nifty100

--- config/universe.py
+++ config/universe.py
@@ -1,3 +1,3 @@
-from config.nifty50 import NIFTY50
+from config.nifty100 import NIFTY100
 
-NSE_STOCKS = ["NIFTYBEES.NS"] + [f"{stock}.NS" for stock in NIFTY50]
+NSE_STOCKS = ["NIFTYBEES.NS"] + [f"{stock}.NS" for stock in NIFTY100]
